d579164de0d6a9e122df7591fde89ae332eb1f2e
Tom Elliott <ipse@paregorios.org>
2022-02-16 06:31:04 -0600

    setup package

--- setup.py
+++ setup.py
@@ -4,21 +4,19 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="{distribution-name}",
+    name="unigaz"
     version="0.0.1",
-    author="{author}",
-    author_email="{author-email}",
-    description="{short-description}",
-    license='MIT',
+    author="Tom Elliott",
+    author_email="tom.elliott@nyu.edu",
+    description="Work with multiple digital humanities gazetteers",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    {project-url}
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: {pyver}",
-        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10.2",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
         "Operating System :: OS Independent",
     ],
     install_requires=['airtight'],
-    python_requires='>={pyver}'
+    python_requires='>=3.10.2'
 )
